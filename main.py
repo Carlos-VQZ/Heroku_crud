2600eb1e4f52033631d5c5f145d61fa6d44c0e03
Juan Carlos Hernandez Vazquez <Carlos-VQZ>
2023-12-12 18:03:53 -0600

    Validacion Por medio de tokens

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import fastapi
 import sqlite3
+import hashlib
+import uuid  # Módulo para generar UUIDs
 from pydantic import BaseModel
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import HTTPBearer
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi import HTTPException, Depends
+from datetime import datetime, timedelta
+
 
 # Crea la base de datos
 conn = sqlite3.connect("contactos.db")
 
 app = fastapi.FastAPI()
 
+# Token
+security = HTTPBearer()
+security_basic = HTTPBasic()
+
+
 class Contacto(BaseModel):
-    email : str
-    nombre : str
-    telefono : str
-    
+    email: str
+    nombre: str
+    telefono: str
+
 # Origins
 origins = [
     "http://localhost:8080",
@@ -20,7 +33,7 @@ origins = [
     "https://herokufrontend23-2e5ad8e49cc5.herokuapp.com"
 ]
 
-# Cors
+# CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -30,10 +43,85 @@ app.add_middleware(
 )
 
 
-# Rutas para las operaciones CRUD
+@app.post("/register/")
+def register(credentials: HTTPBasicCredentials = Depends(security_basic)):
+    if not credentials.username or not credentials.password:
+        raise HTTPException(status_code=401, detail="Acceso denegado: Credenciales faltantes")
+
+    username = credentials.username
+    password = credentials.password
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+    with sqlite3.connect("contactos.db") as conn:
+        c = conn.cursor()
+        c.execute('SELECT username FROM usuarios WHERE username=?', (username,))
+        existing_user = c.fetchone()
+
+        if existing_user:
+            return {"error": "El usuario ya existe"}
+
+        c.execute(
+            'INSERT INTO usuarios (username, password) VALUES (?, ?)',
+            (username, hashed_password)
+        )
+        conn.commit()
+
+    return {"status": "Usuario registrado con éxito"}
+
+@app.get("/login")
+def login(credentials: HTTPBearer = Depends(security)):
+    if not credentials or not credentials.credentials:
+        raise HTTPException(status_code=401, detail="Acceso denegado: Token faltante")
+
+    token = credentials.credentials
+
+    current_timestamp = datetime.utcnow().timestamp()
+
+    with sqlite3.connect("contactos.db") as conn:
+        c = conn.cursor()
+        c.execute('SELECT username, expiration_timestamp FROM usuarios WHERE token=?', (token,))
+        user_data = c.fetchone()
+
+        if user_data and current_timestamp < user_data[1]:  # Verificar si el token está dentro del tiempo de expiración
+            return {"mensaje": "Acceso permitido"}
+        else:
+            raise HTTPException(status_code=401, detail="Acceso denegado: Token inválido o expirado")
+
+@app.get("/token/")
+def generate_token(credentials: HTTPBasicCredentials = Depends(security_basic)):
+    if not credentials.username or not credentials.password:
+        raise HTTPException(status_code=401, detail="Acceso denegado: Credenciales faltantes")
+
+    username = credentials.username
+    password = credentials.password
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+    with sqlite3.connect("contactos.db") as conn:
+        c = conn.cursor()
+        c.execute('SELECT username, password FROM usuarios WHERE username=?', (username,))
+        user = c.fetchone()
+
+        if user and user[1] == hashed_password:
+            timestamp = conn.execute('SELECT strftime("%s", "now")').fetchone()[0]
+            token = hashlib.sha256((username + str(uuid.uuid4())).encode()).hexdigest()
+            expiration_time = timedelta(minutes=120)  # Cambiar la duración a 1 minuto
+            expiration_timestamp = (datetime.utcnow() + expiration_time).timestamp()
+            c.execute(
+                'UPDATE usuarios SET token=?, timestamp=?, expiration_timestamp=? WHERE username=?',
+                (token, timestamp, expiration_timestamp, username)
+            )
+            conn.commit()
+            
+            response_data = {
+                "token": token
+            }
+            return JSONResponse(content=response_data)
+        else:
+            raise HTTPException(status_code=401, detail="Acceso denegado: Credenciales inválidas")
+
 
 @app.post("/contactos")
-async def crear_contacto(contacto: Contacto):
+async def crear_contacto(contacto: Contacto, token: str = Depends(login)):
     """Crea un nuevo contacto."""
     # TODO Inserta el contacto en la base de datos y responde con un mensaje
     c = conn.cursor()
@@ -43,7 +131,7 @@ async def crear_contacto(contacto: Contacto):
     return contacto
 
 @app.get("/contactos")
-async def obtener_contactos():
+async def obtener_contactos(token: str = Depends(login)):
     """Obtiene todos los contactos."""
     # TODO Consulta todos los contactos de la base de datos y los envia en un JSON
     c = conn.cursor()
@@ -54,9 +142,8 @@ async def obtener_contactos():
         response.append(contacto)
     return response
 
-
 @app.get("/contactos/{email}")
-async def obtener_contacto(email: str):
+async def obtener_contacto(email: str, token: str = Depends(login)):
     """Obtiene un contacto por su email."""
     # Consulta el contacto por su email
     c = conn.cursor()
@@ -66,9 +153,8 @@ async def obtener_contacto(email: str):
         contacto = {"email":row[0],"nombre":row[1],"telefono":row[2]}
     return contacto
 
-
 @app.put("/contactos/{email}")
-async def actualizar_contacto(email: str, contacto: Contacto):
+async def actualizar_contacto(email: str, contacto: Contacto, token: str = Depends(login)):
     """Actualiza un contacto."""
     # TODO Actualiza el contacto en la base de datos
     c = conn.cursor()
@@ -77,12 +163,13 @@ async def actualizar_contacto(email: str, contacto: Contacto):
     conn.commit()
     return contacto
 
-
 @app.delete("/contactos/{email}")
-async def eliminar_contacto(email: str):
+async def eliminar_contacto(email: str, token: str = Depends(login)):
     """Elimina un contacto."""
     # TODO Elimina el contacto de la base de datos
     c = conn.cursor()
     c.execute('DELETE FROM contactos WHERE email = ?', (email,))
     conn.commit()
     return {"mensaje":"Contacto eliminado"}
+
+
